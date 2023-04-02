1b9acd415c992d0ef50f434335641ffead913205
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-04-02 08:54:02 +0200

    initial commit

---
+++ src/main/java/de/memozone/SpringShellDemo/commands/HelloCommand.java
@@ -0,0 +1,7 @@
+package de.memozone.SpringShellDemo.commands;
+
+import org.springframework.shell.standard.ShellComponent;
+
+@ShellComponent
+public class HelloCommand {
+}
---
+++ src/test/java/de/memozone/SpringShellDemo/SpringShellDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.memozone.SpringShellDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
